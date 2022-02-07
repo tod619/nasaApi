580be839186aff76fb417a956530795381c9116e
tod619 <trevorodwyer619@hotmail.com>
2022-02-07 21:04:26 +0000

    change text in nav bar in script.js file

--- script.js
+++ script.js
@@ -12,11 +12,20 @@ const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = []
 let favorites = {}
 
-function showContent() {
+function showContent(page) {
     window.scrollTo({
         top: 0,
         behavior: 'instant'
     })
+
+    if(page === 'results') {
+        resultsNav.classList.remove('hidden')
+        favoritesNav.classList.add('hidden')
+    } else {
+        favoritesNav.classList.remove('hidden')
+        resultsNav.classList.add('hidden')
+    }
+
     loader.classList.add('hidden')
 }
 
@@ -87,7 +96,7 @@ function updateDOM(page) {
     }
     imagesContainer.textContent = ''
     createDOMNodes(page)
-    showContent()
+    showContent(page)
 }
 
 // Get 10 images from NASA API
